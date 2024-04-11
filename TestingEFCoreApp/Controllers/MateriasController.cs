2e9b61168104b8cd08bb796d07e50106e218c80e
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-04-11 15:41:58 -0300

    Creacion controlador materias

---
+++ TestingEFCoreApp/Controllers/MateriasController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestingEFCoreApp.Repositories;
+
+namespace TestingEFCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MateriasController : ControllerBase
+    {
+        private readonly IMateriaRepository _materiaRepository;
+
+        public MateriasController(IMateriaRepository materiaRepository)
+        {
+            _materiaRepository = materiaRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetMaterias()
+        {
+            try
+            {
+                var materias = await _materiaRepository.GetAllAsync();
+                return Ok(materias);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetMateriaById(int id)
+        {
+            try
+            {
+                var materia = await _materiaRepository.GetByIdAsync(id);
+                if (materia == null)
+                {
+                    return NotFound();
+                }
+                return Ok(materia);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
