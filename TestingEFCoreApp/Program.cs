28e2790d5f5797b4bbaac039d62d6d444c97b6fc
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-04-11 14:53:01 -0300

    Agrego repositorios al contenedor de servicios

--- TestingEFCoreApp/Program.cs
+++ TestingEFCoreApp/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using TestingEFCoreApp.Data;
+using TestingEFCoreApp.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<EFCoreAppContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("EFCoreAppConnection")));
+
+builder.Services.AddScoped<IMateriaRepository, MateriaRepository>();
+builder.Services.AddScoped<ICarreraRepository, CarreraRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
