1230ffa7a5a0b9173ce3562ece61bf90c79ae26d
Augusto_Robles <Augusto_Robles@itrsa.com.ar>
2024-04-11 14:50:13 -0300

    Implementacion de los repositorios de las entidades Materia y Carrera

---
+++ TestingEFCoreApp/Repositories/CarreraRepository.cs
@@ -0,0 +1,12 @@
+﻿using TestingEFCoreApp.Data;
+using TestingEFCoreApp.Models;
+
+namespace TestingEFCoreApp.Repositories
+{
+    public class CarreraRepository : BaseRepository<Carrera>, ICarreraRepository
+    {
+        public CarreraRepository(EFCoreAppContext appContext) : base(appContext)
+        {
+        }
+    }
+}
---
+++ TestingEFCoreApp/Repositories/MateriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using TestingEFCoreApp.Data;
+using TestingEFCoreApp.Models;
+
+namespace TestingEFCoreApp.Repositories
+{
+    public class MateriaRepository : BaseRepository<Materia>, IMateriaRepository
+    {
+        public MateriaRepository(EFCoreAppContext appContext) : base(appContext)
+        {
+        }
+    }
+}
